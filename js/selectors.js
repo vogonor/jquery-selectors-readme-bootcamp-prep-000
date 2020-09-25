3d141409ade2e089a43b08329db1912f0d76ae1f
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-25 07:24:21 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -25,7 +25,7 @@ function divSelector() {
 
 function firstListItem() {
   
-  var firstLiTtem = $("#pic-list li:first-child")
+  var firstLiTtem = $(".pics #pic-list li:first-child")
    
   return firstListItem
 }
